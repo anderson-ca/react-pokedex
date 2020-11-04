40815a223310b4d8a01807abd319663a1e8d7697
anderson <anderson.d.cardoso92@gmail.com>
2020-11-04 19:09:50 +0400

    add pokemons pictures.

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 const Pokedex = () => {
-
+    let [pokemons, setPokemons] = useState([])
 
     useEffect(() => {
         fetchPokemons()
@@ -12,12 +12,18 @@ const Pokedex = () => {
         const {data} = await axios("https://pokeapi.co/api/v2/pokemon/")
         const pokemons = data.results
         console.log(pokemons)
+        setPokemons(pokemons)
     }
 
     return (
         <div>
             <h2>This is the pokedex</h2>
-
+            {pokemons.map(({name}) => (
+                <div key={name}>
+                    <h2>{name}</h2>
+                    <img src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt="Pokemon picture"/>
+                </div>
+            ))}
         </div>
     );
 }
